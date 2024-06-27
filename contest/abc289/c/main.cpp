e44bf7cc6c125267121448eaae1730809619acae
Takumi Akimoto <akimoto.t.aa@m.titech.ac.jp>
2024-06-27 12:52:50 +0900

    abc289 c

--- contest/abc289/c/main.cpp
+++ contest/abc289/c/main.cpp
@@ -8,32 +8,28 @@ typedef long long ll;
 
 int main() {
   int n, m;
-  vector<int> c(m);
+  cin >> n >> m;
   vector<set<int>> a(m);
   rep(i, m) {
-    cin >> c[i];
-    rep(j, n) {
+    int c;
+    cin >> c;
+    rep(j, c) {
       int input;
       cin >> input;
       a[i].insert(input);
     }
   }
   int cnt = 0;
-  for (int bit = 1; bit <= (1 << m); bit++) {
+  rep(bit, 1 << m) {
     set<int> st;
     rep(i, m) {
       if ((bit >> i) & 1) {
-        st.merge(a[i]);
+        for (auto tmp = a[i].begin(); tmp != a[i].end(); tmp++) {
+          st.insert(*tmp);
+        }
       }
     }
-    bool ok = true;
-    rep(i, n) {
-      if (!st.contains(i + 1)) {
-        ok = false;
-        break;
-      }
-    }
-    if (ok) {
+    if (st.size() == n) {
       cnt++;
     }
   }
