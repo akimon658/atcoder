43ffdccd31fd666f23fc3c94a501f457f9017cbb
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-07-06 11:22:37 +0900

    Fix symlink

--- template.cpp
+++ template.cpp
@@ -1,1 +1,15 @@
-/Users/akimon658/Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace atcoder;
+using namespace std;
+typedef long long ll;
+
+const string el = "\n";
+
+#define all(x) (x).begin(), (x).end()
+#define rep(i, min, sup) for (int i = (int)min; i < (int)sup; i++)
+
+int main() {
+  // Code here
+  return 0;
+}
