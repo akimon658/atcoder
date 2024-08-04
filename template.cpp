8639422e92e47788614dedc73bf08b6504271a63
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-04 12:25:01 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,15 +1,18 @@
 #include <atcoder/all>
 #include <bits/stdc++.h>
+
 using namespace atcoder;
 using namespace std;
+
 typedef long long ll;
 
 const string el = "\n";
 
-#define all(x) (x).begin(), (x).end()
-#define rep(i, min, sup) for (int i = (int)min; i < (int)sup; i++)
+#define all(x) x.begin(), x.end()
+#define rep(i, min, sup)                                                       \
+  for (int i = static_cast<int>(min); i < static_cast<int>(sup); i++)
 
 int main() {
-  // Code here
-  return 0;
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
 }
