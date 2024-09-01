676a22874fd2509502f16c398de40352d92a8ec4
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-01 11:05:29 +0900

    abc369 d

--- contest/abc369/d/main.cpp
+++ contest/abc369/d/main.cpp
@@ -15,4 +15,16 @@ const string el = "\n";
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, 0, n) { cin >> a[i]; }
+  vector<ll> dp_e(n), dp_o(n);
+  dp_e[0] = 0;
+  dp_o[0] = a[0];
+  rep(i, 1, n) {
+    dp_e[i] = max(dp_e[i - 1], dp_o[i - 1] + 2 * a[i]);
+    dp_o[i] = max(dp_e[i - 1] + a[i], dp_o[i - 1]);
+  }
+  cout << max(dp_e[n - 1], dp_o[n - 1]) << el;
 }
