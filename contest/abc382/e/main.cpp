5b5d710b6a3967bff88149d511cb560f1aa7b6b4
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-12-01 09:53:46 +0900

    abc382 e

--- contest/abc382/e/main.cpp
+++ contest/abc382/e/main.cpp
@@ -17,4 +17,30 @@ const string No = "No";
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
+  int n, x;
+  cin >> n >> x;
+  vector<double> p(n);
+  rep(i, 0, n) {
+    double tmp;
+    cin >> tmp;
+    p[i] = tmp / 100;
+  }
+  vector<vector<double>> dp(n + 1, vector<double>(n + 1));
+  vector<double> g(n + 1);
+  dp[0][0] = 1;
+  rep(i, 1, n + 1) { dp[i][0] = (1 - p[i - 1]) * dp[i - 1][0]; }
+  g[0] = dp[n][0];
+  rep(j, 1, n + 1) {
+    rep(i, 1, n + 1) {
+      dp[i][j] = p[i - 1] * dp[i - 1][j - 1] + (1 - p[i - 1]) * dp[i - 1][j];
+    }
+    g[j] = dp[n][j];
+  }
+  vector<double> f(x + 1);
+  rep(i, 1, x + 1) {
+    rep(j, 1, n + 1) { f[i] += g[j] * f[max(i - j, 0)]; }
+    f[i] += 1;
+    f[i] /= 1 - g[0];
+  }
+  cout << setprecision(16) << f[x] << el;
 }
